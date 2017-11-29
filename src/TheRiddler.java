1de0b26fcf7061da7c2ff712a913dc0c1c4ad59e
Cao.Derek <Cao.Derek@SEMS-259-14.SEMS259.LAB>
2017-11-29 14:55:17 -0800

    The Riddler, XGeeks

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,3 +1,6 @@
+import javax.print.attribute.standard.JobMessageFromOperator;
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -6,18 +9,29 @@
 public class TheRiddler {
 
 	public static void main(String[] args) {
+		
+	
 
 		// 1. Make a variable to hold the score
-
+		int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+		String name= JOptionPane.showInputDialog("What word in the dictionary is spelled wrong");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+		if(name.equals("wrong")) {
+			JOptionPane.showMessageDialog(null, "Correct");
+			System.out.println("Correct");
+			score =score+1; 
+		}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+		else {
+			JOptionPane.showMessageDialog(null, "Wrong");
+			System.out.println("Wrong");
+			}
 		// 6. Add some more riddles
-
+		
 		// 2. Make a pop up to show the score.
+		JOptionPane.showMessageDialog(null, "Score = " + score);
+		System.out.println("Score" + score);
 		
 	}
 }
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -4,6 +4,7 @@
 
 
 
+import javax.print.attribute.standard.JobMessageFromOperator;
 import javax.swing.JOptionPane;
 
 /*
@@ -14,12 +15,42 @@ import javax.swing.JOptionPane;
 public class XGeeks {
 public static void main(String[] args) {
 
+String JacobpsPower = "making people salty";
+String JacobjsPower = "teleportation";
+String DereksPower = "god and anime";
+String SanchithasPower = "mind-reader";
+String KellersPower = "telekenesis";
+
+String name = JOptionPane.showInputDialog("Enter a name");
+if(name.equals("Jacobp")) {
+	JOptionPane.showMessageDialog(null, "Your power is " +JacobpsPower);
+	System.out.println("Your power is" +JacobpsPower);
+}
+
+if(name.equals("Jacobj")) {
+	JOptionPane.showMessageDialog(null, "Your power is " +JacobjsPower);
+	System.out.println("Your power is " +JacobjsPower);
+}
+
+if(name.equals("Keller")) {
+	JOptionPane.showMessageDialog(null, "Your power is " +KellersPower);
+	System.out.println("Your power is " +KellersPower);
+}
+
+if(name.equals("Sanchitha")) {
+	JOptionPane.showMessageDialog(null, "Your power is " +SanchithasPower);
+	System.out.println("Your power is " +SanchithasPower);
+}
+
+if(name.equals("Derek")) {
+	JOptionPane.showMessageDialog(null, "Your power is " +DereksPower);
+	System.out.println("Your power is " +DereksPower);
+}
 		// 1. Save the superpower for each person in a variable.
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
 
-	}
 }
-
+}
